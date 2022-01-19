6b4e9f6b46d3d81c5629c75983463351330a0d90
Matthew <matt.cavallo56@gmail.com>
2022-01-19 10:14:56 -0600

    Add project files.

---
+++ OptionsSampleCore/App.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace OptionsSampleCore
+{
+    public class App
+    {
+        private readonly ILogger<App> _logger;
+        private readonly AppSettings _appSettings;
+
+        public App(IOptionsMonitor<AppSettings> appSettings, ILogger<App> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _appSettings = appSettings?.CurrentValue ?? throw new ArgumentNullException(nameof(appSettings));
+        }
+
+        public async Task Run(string[] args)
+        {
+            _logger.LogInformation("Starting...");
+
+            while (true)
+            {
+                Console.WriteLine("App Start!");
+                Console.WriteLine(_appSettings.TempDirectoryPath);
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            _logger.LogInformation("App Finished!");
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ OptionsSampleCore/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptionsSampleCore
+{
+    public class AppSettings
+    {
+        public string TempDirectoryPath { get; set; }
+    }
+}
